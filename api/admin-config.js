e6f6853fe7cd3e5e330112c0640be6cc657ba11b
Maxi Dottori <maxi.dottori@gmail.com>
2026-08-30 11:07:45 -0300

    Remove temp admin migration endpoints

--- api/admin-config.js
+++
@@ -1,9 +0,0 @@
-import { initDb } from "./_firebase.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") return res.status(405).end();
-  const db = initDb();
-  const snap = await db.collection("config").doc("expedicion").get();
-  const data = snap.data() || {};
-  return res.status(200).json({ armadores: data.armadores || [] });
-}
